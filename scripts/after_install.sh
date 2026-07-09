98dc4194f61bff9d0e7ea0582b8f04d84d1f0007
Muhammad Zain <zainuet2022@gmail.com>
2026-07-09 11:54:50 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-cd /var/www/html/MyApplication
+cd /var/www/html/MyApplication/
 
 composer install --no-dev --optimize-autoloader
+php artisan migrate 
 
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan view:clear
+php artisan optimize:clear
 
-php artisan migrate --force
+sudo chown -R apache:apache /var/www/html/MyApplication
+sudo chmod -R 775 /var/www/html/MyApplication/storage
+sudo chmod -R 775 /var/www/html/MyApplication/bootstrap/cache
 
-php artisan config:cache
-php artisan route:cache
-
-chown -R ec2-user:apache /var/www/html/MyApplication
-chmod -R 775 storage bootstrap/cache
