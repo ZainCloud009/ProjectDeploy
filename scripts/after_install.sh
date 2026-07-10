fdc67bdf125ff8f109c3c11800813714772c9bc3
Muhammad Zain <zainuet2022@gmail.com>
2026-07-11 00:43:34 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,25 +2,9 @@
 set -e
 
 cd /var/www/html/app
-composer install 
 
-echo "Setting permissions..."
-chown -R ec2-user:apache /var/www/html
-chmod -R 775 /var/www/html/app/storage
-chmod -R 755 /var/www/html/app/bootstrap/cache
+sudo chown -R ec2-user:apache /var/www/html
+sudo chmod 2775 /var/www/html/lab && find /var/www/html/lab -type d -exec sudo chmod 2775 {} \;
+find /var/www/html/app -type f -exec sudo chmod 0664 {} \;
 
-echo "Generating app key..."
-php artisan key:generate 
-
-echo "Clearing cache..."
-php artisan optimize:clear
-
-echo "Running migrations..."
-php artisan migrate 
-
-echo "Caching config..."
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-echo "Deployment completed successfully."
+sudo systemctl restart httpd
