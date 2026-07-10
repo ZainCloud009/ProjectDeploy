b72a99b6a43bcfe13bd0f135202a61b712486cc4
Muhammad Zain <zainuet2022@gmail.com>
2026-07-11 00:39:20 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,9 +2,7 @@
 set -e
 
 cd /var/www/html/app
-
-echo "Installing composer dependencies..."
-composer install --no-dev --optimize-autoloader
+composer install 
 
 echo "Setting permissions..."
 chown -R ec2-user:apache /var/www/html
@@ -12,13 +10,13 @@ chmod -R 775 /var/www/html/app/storage
 chmod -R 755 /var/www/html/app/bootstrap/cache
 
 echo "Generating app key..."
-php artisan key:generate --force
+php artisan key:generate 
 
 echo "Clearing cache..."
 php artisan optimize:clear
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate 
 
 echo "Caching config..."
 php artisan config:cache
