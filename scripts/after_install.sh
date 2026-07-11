0534f1db831649104fb7544fda3d96d3b6afc8c9
Muhammad Zain <zainuet2022@gmail.com>
2026-07-11 13:40:38 +0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-cd /var/www/html/app
-
-sudo chown -R ec2-user:apache /var/www/html
-sudo chmod 2775 /var/www/html/app && find /var/www/html/app -type d -exec sudo chmod 2775 {} \;
-find /var/www/html/app -type f -exec sudo chmod 0664 {} \;
 
 sudo systemctl restart httpd
